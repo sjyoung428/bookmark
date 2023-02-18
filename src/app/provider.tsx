092361708c21a68ce6818285d1901a6d73f3023d
sjyoung428 <sjy8267@naver.com>
2023-02-18 21:20:43 +0900

    feat: set QueryClientProvider

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { SessionProvider } from "next-auth/react";
 import AuthWrapper from "@/components/AuthWrapper";
 import StyledComponentsRegistry from "@/lib/registry";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export const Providers = ({ children }: Props) => {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <>
       <StyledComponentsRegistry>
-        <SessionProvider refetchOnWindowFocus={false}>
-          <AuthWrapper>
-            <>{children}</>
-          </AuthWrapper>
-        </SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          <SessionProvider refetchOnWindowFocus={false}>
+            <AuthWrapper>
+              <>{children}</>
+            </AuthWrapper>
+          </SessionProvider>
+        </QueryClientProvider>
       </StyledComponentsRegistry>
     </>
   );
